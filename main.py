1ec86eee8e400723a38b6f70142a3dbf331405f9
Alex Howard <99138808+Alex-M-Howard@users.noreply.github.com>
2022-06-05 12:32:07 -0400

    Removed some lines that were commented out
    During intial testing, had more lines to distinguish difference between file name, file extension, and file path.

--- main.py
+++ main.py
@@ -2,24 +2,22 @@ import os
 from utilities import check_file_existence, move_file
 
 HARD_DRIVE = 'C'
-USERNAME = 'alexa'
-TEMPORARY_FILES = ['crdownload', 'tmp']
+USERNAME = 'alexa'                                 # Change the hard_drive letter and username to match your machine!
+TEMPORARY_FILES = ['crdownload', 'tmp']            # Add whatever temp files you have seen. This ignores them while something is STILL downloading
 PATH = f'{HARD_DRIVE}:/Users/{USERNAME}/Downloads'
 
 for file in os.listdir(PATH):
     try:
         if os.path.isfile(os.path.join(PATH, file)):
             file_name = file[::-1].split('.')
-            extension = file_name[0][::-1]             # Getting file name and extension
-            #file_name = file.split(f'.{extension}')    # Can probably be simplified, will learn later
-            #file_name = f'{file_name[0]}.{extension}'
+            extension = file_name[0][::-1]              # Getting file name and extension
             file_name = file
             file_path = f'{PATH}/{file_name}'
 
             if extension not in TEMPORARY_FILES:
-                file_name, file_path = check_file_existence(file_name, file_path, PATH)
+                file_name, file_path = check_file_existence(file_name, file_path, PATH)     # Deals with duplicates by adding a 0 to front of filename
                 move_file(file_name, extension, file_path, PATH)
         else:
-            continue
+            continue                                    # Ignores any extra folders that you've placed in your downloads folder
     except:
         pass
